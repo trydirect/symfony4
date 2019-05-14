729c9ee93021efc79c38dec0817de3ac0c1a5fdb
vsilent <jabberroid@gmail.com>
2019-05-14 17:15:02 +0300

    other containers simple test

--- tests.py
+++ tests.py
@@ -13,8 +13,48 @@ client = docker.from_env()
 
 time.sleep(20)  # we expect all containers are up and running in 20 secs
 for c in client.containers.list():
-    print(c.name)
-    print(c.status)
+    assert c.status == 'running'
+
+# NGINX
+nginx = client.containers.get('nginx')
+nginx_cfg = nginx.exec_run("/usr/sbin/nginx -T")
+assert nginx.status == 'running'
+assert 'server_name symf.com;' in nginx_cfg.output.decode()
+assert 'server_name kibana.symf.com;' in nginx_cfg.output.decode()
+assert "proxy_pass http://kibana:5601;" in nginx_cfg.output.decode()
+assert "error_log /proc/self/fd/2" in nginx_cfg.output.decode()
+assert "location = /.well-known/acme-challenge/" in nginx_cfg.output.decode()
+assert 'HTTP/1.1" 500' not in nginx.logs()
+
+# test restart
+nginx.restart()
+time.sleep(3)
+assert nginx.status == 'running'
+
+# Kibana
+kibana = client.containers.get('kibana')
+assert kibana.status == 'running'
+response = requests.get("http://127.0.0.1:5601")
+assert response.status_code == 200
+assert "var hashRoute = '/app/kibana'" in response.text
+assert '"statusCode":404,"req":{"url":"/elasticsearch/logstash-' not in kibana.logs()
+
+# Logstash
+logstash = client.containers.get('logstash')
+assert logstash.status == 'running'
+response = requests.get("http://127.0.0.1:5044")
+assert response.status_code == 200
+assert "" in response.text
+assert 'Successfully started Logstash API endpoint {:port=>9600}' in logstash.logs()
+assert 'Pipeline main started' in logstash.logs()
+
+# Elasticsearch
+elastic = client.containers.get('elasticsearch')
+assert elastic.status == 'running'
+response = requests.get("http://127.0.0.1:9200")
+assert response.status_code == 200
+assert '"number" : "5.4.3"' in response.text
+assert ' bound_addresses {0.0.0.0:9200}' in elastic.logs()
 
 # Symfony PHP
 php = client.containers.get('php')
@@ -27,14 +67,37 @@ php_proc = php.exec_run("ps aux |grep php-fpm")
 assert 'php-fpm: master process (/usr/local/etc/php-fpm.conf)' in php_proc.output.decode()
 assert 'fpm is running, pid' in php.logs()
 
-mysql = client.containers.get('db')
-
-assert mysql.status == 'running'
-mycnf = mysql.exec_run("/usr/sbin/mysqld —verbose —help")
-assert '' in mysql.output.decode()
-mysql_log = mysql.logs()
-print(mysql_log.decode())
-assert "Ready to accept connections" in mysql_log.decode()
-
 #response = requests.get("http://127.0.0.1:9000")
 #assert response.status_code == 200
+
+redis = client.containers.get('redis')
+assert redis.status == 'running'
+redis_cli = redis.exec_run("redis-cli ping")
+assert 'PONG' in redis_cli.output.decode()
+redis_log = redis.logs()
+assert "Ready to accept connections" in redis_log.decode()
+
+# mysql = client.containers.get('db')
+# assert mysql.status == 'running'
+# mycnf = mysql.exec_run("/usr/sbin/mysqld --verbose  --help")
+# assert '/usr/sbin/mysqld  Ver 5.7.26' in mycnf.output.decode()
+# mysql_log = mysql.logs()
+# assert "Ready to accept connections" in mysql_log.decode()
+
+db = client.containers.get('db')
+assert db.status == 'running'
+cnf = db.exec_run('psql -h 127.0.0.1 -p 5932 -c "select 1"')
+print(cnf.output.decode())
+# assert '' in cnf.output.decode()
+log = db.logs()
+# assert "Ready to accept connections" in log.decode()
+
+smtp = client.containers.get('symfony_smtp_1')
+assert smtp.status == 'running'
+smtp_log = smtp.logs()
+assert '' in smtp_log.decode()
+
+mq = client.containers.get('mq')
+assert mq.status == 'running'
+logs = mq.logs()
+assert 'Server startup complete; 3 plugins started' in logs.decode()
